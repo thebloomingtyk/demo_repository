423e1bb3ad0203fbec2c03f1379d3653bab424b3
thebloomingtyk <88451926+thebloomingtyk@users.noreply.github.com>
2023-07-22 14:51:30 +0300

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
-
-# Simple Interest Calculator
-
-# Function to calculate simple interest
-calculate_simple_interest() {
-    principal=$1
-    rate=$2
-    time=$3
-
-    interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
-    echo "Simple Interest: \$ $interest"
-}
-
-# Main script
-
-# Check if all required arguments are provided
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <principal_amount> <interest_rate> <time_in_years>"
-    exit 1
-fi
-
-principal_amount=$1
-interest_rate=$2
-time_in_years=$3
-
-# Check if the provided arguments are valid numbers
-if ! [[ $principal_amount =~ ^[0-9]+([.][0-9]+)?$ && $interest_rate =~ ^[0-9]+([.][0-9]+)?$ && $time_in_years =~ ^[0-9]+([.][0-9]+)?$ ]]; then
-    echo "Error: Invalid input. Please provide valid numerical values for principal_amount, interest_rate, and time_in_years."
-    exit 1
-fi
-
-# Calculate and display the simple interest
-calculate_simple_interest $principal_amount $interest_rate $time_in_years
+   # This script calculates simple interest given principal,
+   # annual rate of interest and time period in years.
+   # Do not use this in production. Sample purpose only.
+   # Author: Upkar Lidder (IBM)
+   # Additional Authors:
+   # <your GitHub username>
+   # Input:
+   # p, principal amount
+   # t, time period in years
+   # r, annual rate of interest
+   # Output:
+   # simple interest = p*t*r
+   echo "Enter the principal:"
+   read p
+   echo "Enter rate of interest per year:"
+   read r
+   echo "Enter time period in years:"
+   read t
+   s=`expr $p \* $t \* $r / 100`
+   echo "The simple interest is: "
+   echo $s
